1d2cb4c3ac2a577000769fb65ec7a15c3a7ac473
shoyu777 <shoyu7@gmail.com>
2022-09-24 00:05:19 +0900

    update: css setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://shoyu777.github.io",
+  site: "https://shoyu777.github.io/zerolinesjs-site",
 });
